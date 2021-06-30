4aded582b52a4788fae10d2ab259338e9ea60387
karlis pcwork <karlita.valdivieso@gmail.com>
2021-06-30 10:46:49 -0400

    correccion link JS

---
+++ main.js
@@ -0,0 +1,12 @@
+let number = 15;
+/*let num2=0;*/
+
+let nombre = prompt("ingrese su nombre");
+alert("hola " + nombre + " !")
+num2 = parseInt(prompt("ingrese su numero"))
+let resultado = number + num2;
+alert("si los sumas a 15 da " + resultado)
+alert("ahora para concatenar apriete aceptar")
+let concat1 = prompt("ingrese un string ")
+let concat2 = prompt("ingrese string 2 ")
+alert("su cadena es " + concat1 + concat2)
