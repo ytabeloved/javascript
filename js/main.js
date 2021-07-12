2e8e414727b4b5011e664dccdc5a922a69a487ed
karlis pcwork <karlita.valdivieso@gmail.com>
2021-07-12 12:00:32 -0400

    desafio adicional 4

--- js/main.js
+++ js/main.js
@@ -1,39 +1,54 @@
-/*LOOPS PARTE 1 */
-let num = parseInt(prompt("ingrese un numero para este loop que correra solo 10 veces"));
-let i = 0;
-let resultado = 0;
-/*cuerpo del loop */ 
-while (i < 10) {
-    if (i != 9) {
-        resultado = resultado + num;
-        alert("la suma es " + resultado);
-        num = parseInt(prompt("Agregue un numero para el loop que correra " + (-i + 9) + " veces"));
-        i++;
-    } else {
-        resultado = resultado + num;
-        alert("la suma es " + resultado);
-        alert("no puedes agregar mas numeros");
-        i++;
+/*complementario desafio 4 */
+
+/*funcion 1, es par o impar */
+
+function isOdd(num) {
+    if (num % 2 === 0) { //si el remainder es cero es par
+        alert("es numero par");
+        return true;
     }
+    alert("es numero impar");
+    return false;
 }
 
-/*LOOP 2 STRINGS */
+let num = prompt("ingrese un numero para validar si es par o impar: ");
+isOdd(num);
 
-let string1 = prompt("ingrese mientras quiera un string a la cadena, para salir escriba ESC en mayusculas");
-let cadena = " ";
-/*cuerpo loop 2 */
-while(string1 != "ESC"){
-    cadena = cadena + " " + string1;
-    alert("su concatenacion es: " + cadena);
-    string1 = prompt("ingrese su prox string para concatenar o escriba ESC en mayuscula para terminar");
+/* funcion 2, funcion del eco */
+
+let str = prompt('ingrese la palabra que quiere escuchar en eco, con la primera letra en mayuscula por favor!');
+
+function eco(str) {
+    let dots = ' ... '
+    alert(str.toUpperCase() + dots + str + dots + str.toLowerCase()); //imprime en eco las palabras
+}
+eco(str);
+
+/*funcion 3 son primos*/
+
+let isPrime = function(num1) {
+    if (num1 < 2) { //validamos si es menor que 2
+        return false;
+    }
+    for (let i = 2; i < num1; i++) {//validamos que si es mayor que 2 mientras el remainder sea igual a cero devuelva falso
+        if (num1 % i === 0) {
+            return false;
+        }
+    }
+    return true;//si los dos anteriores son falsos entonces es primo
+};
+
+let primeFactors = function(num1) {
+    let primeFacts = [];//creamoas array para guardar los factores
+    for (let i = 1; i <= num1; i++) {//iteramos por todos los numeros desde cero hasta el numero elegido
+        if (num1 % i === 0 && isPrime(i)) {//si podemos dividir el numero elegido por i y su remainder es cero lo metemos al array como factor
+            primeFacts.push(i);
+        }
+    }
+    alert('los factores primos son: ' + primeFacts);
+    return primeFacts;
 }
 
-/*LOOP 3 */
+let num1 = prompt('ingrese un numero y le mostraremos todos sus factores primos');
+primeFactors(num1);
 
-let num2=parseInt(prompt("ingrese cuantas veces quiere que lo saludemos!"));
-
-/*cuerpo del loop */
-
-for(let i2=1; i2<=num2; i2++){
-    alert("hola!,   por " + i2 +"a vez")
-}
