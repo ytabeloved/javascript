c40da7805f34bd5cb884909bbfa74df23c991e2b
karlis pcwork <karlita.valdivieso@gmail.com>
2021-07-20 17:49:05 -0400

    creacion de array para luego filtrar

--- js/main.js
+++ js/main.js
@@ -1,75 +1,14 @@
-/*desafio 5 objetos */
-class Prestamo {
-    constructor(amount, cuota, interes, pagos) { //constructor del objeto prestamo
-        this.amount = parseFloat(amount);
-        this.cuota = parseFloat(cuota);
-        this.interes = parseFloat(interes);
-        this.pagos = parseFloat(pagos);
+//crear un array con multiples palabras
+let frase = prompt("ingrese una frase de multiples palabras separadas por espacio:   ") //pide una frase
 
-    }
-    calculoPrestamo() {
-        this.pagos = pagos(this.amount, this.cuota);
-        return this.pagos;
-    }
-
+//funcion que crea el array 
+let creaArray = function (str) {
+    let Arr = str.split(" ");
+    return Arr;
 }
 
+let arr = creaArray(frase);
 
-//crea el array
-let client1 = [];
-
-
-
-//inserta las var en el array
-let crearCuenta = prompt("desea un prestamo si o no?");
-if (crearCuenta.toLowerCase() === "si") {
-    let newAmount = prompt('monto a pedir prestado: '); //define la captura de datos
-    let newCuotas = prompt('en cuantas cuotas? : ');
-    let newInteres = prompt('a que tasa de interes? (solo ingrese un numero entero): ');
-
-    client1.push(newAmount, newCuotas, newInteres) //graba los valores en el array 
-} else {
-    alert("vuelva cuando desee un credito")
-}
-
-
-
-/*creacion de objeto*/
-
-let prestamo1 = new Prestamo(client1[0, 0], client1[0, 1], client1[0, 2]);
-console.log(client1[0, 0])
-console.log(client1[0, 1])
-console.log(client1[0, 2])
-
-/*funciones del simulador  */
-
-function tasa(interes) { //convierte interes en porcentaje
-    let perc = prestamo1.interes / 100;
-    // console.log(perc);
-
-    return perc;
-}
-
-let pagos = function (amount, cuota) { //calcula el monto a pagar x cuota
-    let pagos = ((prestamo1.amount * tasa(prestamo1.interes)) + prestamo1.amount) / prestamo1.cuota;
-    return pagos;
-}
-
-/*llamadas de consola para control*/
-console.log(client1);
-console.log(prestamo1.calculoPrestamo());
-console.log(tasa(prestamo1.interes));
-console.log(pagos(prestamo1.amount, prestamo1.cuota, tasa(prestamo1.interes)));
-
-//llamada funcion y adios
-let resultado = prestamo1.calculoPrestamo();
-console.log("tipo :" + resultado)
-if (isNaN(resultado)) {
-    alert("Adios!")
-} else {
-    alert('su cuota quedaria en: $' + prestamo1.calculoPrestamo()); //llamada del metodo
-    alert("resumen del prestamo:  " + "monto pedido: " + client1[0, 0] + " en " +
-        client1[0, 1] + " cuotas, " + "con una tasa de: " + client1[0, 2] + "%")
-
-}
-
+//llama a la funcion
+console.log(arr)
+alert("tu array es [" + creaArray(frase) + "]");
