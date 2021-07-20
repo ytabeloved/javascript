05d363d47d22e0065a32f5a199e47628eb984f7c
karlis pcwork <karlita.valdivieso@gmail.com>
2021-07-20 17:32:10 -0400

    desafio 6 incluir arrays de manera muy creativa :(

--- js/main.js
+++ js/main.js
@@ -5,43 +5,70 @@ class Prestamo {
         this.cuota = parseFloat(cuota);
         this.interes = parseFloat(interes);
         this.pagos = parseFloat(pagos);
-       
+
     }
-    calculoPrestamo(){
+    calculoPrestamo() {
         this.pagos = pagos(this.amount, this.cuota);
         return this.pagos;
     }
-   
+
 }
 
 
+//crea el array
+let client1 = [];
+
+
+
+//inserta las var en el array
+let crearCuenta = prompt("desea un prestamo si o no?");
+if (crearCuenta.toLowerCase() === "si") {
+    let newAmount = prompt('monto a pedir prestado: '); //define la captura de datos
+    let newCuotas = prompt('en cuantas cuotas? : ');
+    let newInteres = prompt('a que tasa de interes? (solo ingrese un numero entero): ');
+
+    client1.push(newAmount, newCuotas, newInteres) //graba los valores en el array 
+} else {
+    alert("vuelva cuando desee un credito")
+}
 
 
 
 /*creacion de objeto*/
 
-let prestamo1 = new Prestamo(prompt('monto a pedir prestado: ') , prompt('en cuantas cuotas? : '), prompt('a que tasa de interes? (solo ingrese un numero entero): ') );
+let prestamo1 = new Prestamo(client1[0, 0], client1[0, 1], client1[0, 2]);
+console.log(client1[0, 0])
+console.log(client1[0, 1])
+console.log(client1[0, 2])
 
 /*funciones del simulador  */
 
 function tasa(interes) { //convierte interes en porcentaje
     let perc = prestamo1.interes / 100;
-   // console.log(perc);
-    return perc;  
+    // console.log(perc);
+
+    return perc;
 }
 
 let pagos = function (amount, cuota) { //calcula el monto a pagar x cuota
-    let pagos = ((prestamo1.amount * tasa(prestamo1.interes))+prestamo1.amount)/ prestamo1.cuota;
+    let pagos = ((prestamo1.amount * tasa(prestamo1.interes)) + prestamo1.amount) / prestamo1.cuota;
     return pagos;
 }
 
 /*llamadas de consola para control*/
-//console.log(prestamo1.amount); //control de valores
-//console.log(prestamo1.cuota);
+console.log(client1);
 console.log(prestamo1.calculoPrestamo());
 console.log(tasa(prestamo1.interes));
 console.log(pagos(prestamo1.amount, prestamo1.cuota, tasa(prestamo1.interes)));
-//console.log('tu monto a pagar es: $ ' + pagos(prestamo1.amount, prestamo1.cuota, tasa(prestamo1.interes)));
 
+//llamada funcion y adios
+let resultado = prestamo1.calculoPrestamo();
+console.log("tipo :" + resultado)
+if (isNaN(resultado)) {
+    alert("Adios!")
+} else {
+    alert('su cuota quedaria en: $' + prestamo1.calculoPrestamo()); //llamada del metodo
+    alert("resumen del prestamo:  " + "monto pedido: " + client1[0, 0] + " en " +
+        client1[0, 1] + " cuotas, " + "con una tasa de: " + client1[0, 2] + "%")
 
-alert('su cuota quedaria en: $' + prestamo1.calculoPrestamo()); //llamada del metodo
+}
